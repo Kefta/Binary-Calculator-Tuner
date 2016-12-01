5c14cc40187f4dfb9f9f16cfffd4ec7a9783d5e3
Collin (code_gs) <collings509@gmail.com>
2016-12-01 13:02:10 -0600

    Create Tuner.java

---
+++ Tuner.java
@@ -0,0 +1,68 @@
+package code_gs.binarycalculatortuner;
+
+public class Tuner
+{
+    private static final double PEAK_HEIGHT = 0.7;
+
+    static double getFrequency(int rate, int size, int count, short[] buffer)
+    {
+        int[] condensed = new int[size/2];
+
+        // Each point takes up two shorts, combine them for straight comparison
+        for (int i = 0; i < count; i += 2)
+        {
+            condensed[i / 2] = (buffer[i] & 0xFFFF) | ((buffer[i] & 0xFFFF) << 16);
+        }
+
+        double prevPeak = 0;
+        double prevDx = 0;
+        double maxDist = 0;
+
+        int sample = 0;
+        int len = condensed.length / 2;
+
+            for (int i = 0; i < len; i++) {
+                double dist = 0;
+
+                for (int j = 0; j < len; j++)
+                {
+                    // Compare the base wave with one offset by horizontal translation i
+                    dist += Math.abs(condensed[j] - condensed[i + j]);
+                }
+
+                double dx = prevPeak - dist;
+
+                // Change of sign in dx, only check peaks
+                if (dx < 0 && prevDx > 0 && dist < PEAK_HEIGHT * maxDist && sample <= 0)
+                {
+                    sample = i - 1;
+                }
+
+                prevDx = dx;
+                prevPeak = dist;
+                maxDist = Math.max(dist, maxDist);
+            }
+
+        if (sample > 0)
+        {
+            double frequency = (rate/sample);
+            //Log.d("Calculator", String.format("%.2fhz",frequency));
+
+            // Put hz into an octave range
+            // A4
+            while (frequency < 440) {
+                frequency *= 2;
+            }
+
+            // A5
+            while (frequency > 880) {
+                frequency /= 2;
+            }
+
+            return frequency;
+        }
+
+        // No clear peaks could be found; sound too varied or not varied enough
+        return -1;
+    }
+}
